e1bd66bb4f8910dbc5e6723161b235f12a1a84f4
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-27 17:59:44 -0600

    Ejercicios Basicos 1

---
+++ Java/1.- Intro/BasicJava.java
@@ -0,0 +1,16 @@
+public class BasicJava {
+    /* Métodos */
+
+    public static void imprimir1255() {
+        for(int i = 1; i <= 255; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void numerosImpares() {
+        for(int i=1; i <= 255; i+=2) {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ Java/1.- Intro/BasicJavaTest.java
@@ -0,0 +1,8 @@
+public class BasicJavaTest {
+    /* Método principal. Llamadas a los métodos de BasicJava */
+    public static void main(String[] args) {
+        //Llamadas
+        //BasicJava.imprimir1255();
+        BasicJava.numerosImpares();
+    }
+}
