69eef6bfc7c25f01e1699c9fe98f292cde0aaa6b
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-28 15:47:59 -0600

    OOP

---
+++ Java/2.- OOP/Vehiculo.java
@@ -0,0 +1,91 @@
+class Vehiculo {
+
+    //Atributos o Variables miembro
+    private int anio;
+    private String marca;
+    private String modelo;
+    private String color;
+    private int ruedas;
+
+    /* Método Constructor: es el encargado de inicializar mi objeto */
+    public Vehiculo() {
+        //NO inicializo ningún valor
+    }
+
+    /* SOBRECARGA de métodos constructores 
+    this = auto3
+    anio = 1991
+    marca = Renault
+    modelo = 12
+    color = naranja
+    ruedas = 4
+    */
+    public Vehiculo(int anio, String marca, String modelo, String color, int ruedas){
+        //auto3.anio = 1991
+        this.anio = anio; 
+        //auto3.marca = Renault
+        this.marca = marca;
+        //auto3.modelo = 12
+        this.modelo = modelo;
+        //auto3.color = naranja
+        this.color = color;
+        //auto3.ruedas = 4
+        this.ruedas = ruedas;
+    }
+
+    public Vehiculo(int anio, String marca, String modelo, int ruedas){
+        this.anio = anio; 
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = "blanco";
+        this.ruedas = ruedas;
+    }
+
+    //Método de los objetos Vehiculo
+    public String toString() {
+        return "{Anio:"+this.anio+"\n Modelo:"+this.modelo+"\n Marca:"+this.marca+" \nColor:"+this.color+"\n Ruedas:"+this.ruedas+"}";
+    }
+
+    /* Como mis atributos son privados, necesito métodos Getters y Setters. Get se encarga de obtener la info. Set se encarga de guardar nueva info */
+
+    public int getAnio() {
+        return anio;
+    }
+
+    //anio = 2001
+    public void setAnio(int anio){
+        this.anio = anio; //auto1.anio = 2001
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public int getRuedas(){
+        return ruedas;
+    }
+
+    public void setRuedas(int ruedas){
+        this.ruedas = ruedas;
+    }
+
+}
---
+++ Java/2.- OOP/VehiculoTest.java
@@ -0,0 +1,51 @@
+class VehiculoTest {
+
+    public static void main(String[] args){
+
+        //Crear un nuevo vehiculo
+        Vehiculo auto1 = new Vehiculo();
+        //auto1.anio = 2001;
+        auto1.setAnio(2001);
+        //auto1.marca = "Honda";
+        auto1.setMarca("Honda");
+        //auto1.modelo = "Civic";
+        auto1.setModelo("Civic");
+        //auto1.color = "blanco";
+        auto1.setColor("blanco");
+        //auto1.ruedas = 4;
+        auto1.setRuedas(4);
+
+        System.out.println("Marca auto1:"+auto1.getMarca());
+
+        //nuevo Vehiculo
+        Vehiculo auto2 = new Vehiculo();
+        //auto2.anio = 2002;
+        auto2.setAnio(2002);
+        //auto2.marca = "Toyota";
+        auto2.setMarca("Toyota");
+        //auto2.modelo = "Supra";
+        auto2.setModelo("Supra");
+        //auto2.color = "azul";
+        auto2.setColor("azul");
+        //auto2.ruedas = 4;
+        auto2.setRuedas(4);
+
+        System.out.println("Modelo auto2:"+auto2.getModelo());
+
+        Vehiculo auto3 = new Vehiculo(1991, "Renault","12", "naranja", 4);
+
+        Vehiculo auto4 = new Vehiculo(2024, "BMW", "M4", "negro", 4);
+
+        System.out.println("Color auto3:"+auto3.getColor());
+
+        System.out.println("Marca auto4:"+auto4.getMarca());
+
+        Vehiculo moto1 = new Vehiculo(1998, "Yamaha", "YZF-R1", 2); //Defecto color = "blanco"
+        System.out.println("Color moto1:"+moto1.getColor());
+
+        System.out.println(moto1);
+        System.out.println(auto4); //Ante un obj busca el método toString.
+
+    }
+
+}
