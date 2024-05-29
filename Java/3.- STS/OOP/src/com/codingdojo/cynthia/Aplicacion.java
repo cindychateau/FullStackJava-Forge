a5c3e4febd503eb5472d6ab5eef5cfd42233cfad
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-29 17:32:01 -0600

    STS

---
+++ Java/3.- STS/OOP/src/com/codingdojo/cynthia/Aplicacion.java
@@ -0,0 +1,19 @@
+package com.codingdojo.cynthia;
+
+import java.util.ArrayList;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vehiculo auto1 = new Vehiculo(2010, "Honda", "Civic", "Rojo", 4);
+		System.out.println("Marca Auto1: "+auto1.getMarca());
+		System.out.println(auto1);
+		
+		/*Para agregar importación: Ctrl + Shift + O*/
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		
+	}
+
+}
