98f198d4f68c6b70e65d70c590ceb2da0927e03d
Cynthia Castillo <ca.castillo@gmail.com>
2024-05-30 17:23:33 -0600

    Guardia del Zoologico1

---
+++ Java/3.- STS/GuardiaZoologico/src/com/codingdojo/cynthia/Mammal.java
@@ -0,0 +1,12 @@
+package com.codingdojo.cynthia;
+
+public class Mammal {
+
+	protected int energyLevel = 100;
+	
+	public int displayEnergy() {
+		System.out.println("Nivel de energia: "+energyLevel);
+		return energyLevel;
+	}
+	
+}
