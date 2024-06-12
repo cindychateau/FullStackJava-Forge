61f00ab344f4c20d1ec2df608071496d2d828470
Cynthia Castillo <ca.castillo@gmail.com>
2024-06-11 18:44:27 -0600

    Usuario API

--- Java/3.- STS/ProyectoUsuarios/src/main/java/com/codingdojo/cynthia/controladores/ControladorAPI.java
+++ Java/3.- STS/ProyectoUsuarios/src/main/java/com/codingdojo/cynthia/controladores/ControladorAPI.java
@@ -3,8 +3,11 @@ package com.codingdojo.cynthia.controladores;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +36,26 @@ public class ControladorAPI {
 		Usuario nuevoUsuario = new Usuario(nombre, apellido, email, password); //Creo la instancia de Usuario
 		return serv.guardarUsuario(nuevoUsuario);
 		
+	}
+	
+	@DeleteMapping("/{id}") // /api/usuarios/1
+	public void apiBorrarUsuario(@PathVariable("id") Long id) {
+		serv.borrarUsuario(id);
+	}
+	
+	@GetMapping("/{id}") // /api/usuarios/2
+	public Usuario apiMostrarUsuario(@PathVariable("id") Long id) {
+		return serv.buscarUsuario(id);
+	}
+	
+	@PutMapping("/{id}")
+	public Usuario apiEditarUsuario(@PathVariable("id") Long id,
+									@RequestParam("nombre") String nombre,
+									@RequestParam("apellido") String apellido,
+									@RequestParam("email") String email,
+									@RequestParam("password") String password) {
+		Usuario usuarioActualizado = new Usuario(id, nombre, apellido, email, password);
+		return serv.guardarUsuario(usuarioActualizado);
 	}
 	
 }
