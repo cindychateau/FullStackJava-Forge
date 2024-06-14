3d74d159f1359b9df0f4f7b429e3c85d259c39c5
Cynthia Castillo <ca.castillo@gmail.com>
2024-06-13 18:02:17 -0600

    Inicio de Lenguajes

---
+++ Java/3.- STS/Lenguajes/src/main/java/com/codingdojo/cynthia/modelos/Language.java
@@ -0,0 +1,108 @@
+package com.codingdojo.cynthia.modelos;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="languages")
+public class Language {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 2, max = 20)
+	private String name;
+	
+	@NotNull 
+	@Size(min = 2, max = 20)
+	private String creator;
+	
+	@NotEmpty
+	private String currentVersion;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+
+	public Language() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public String getCurrentVersion() {
+		return currentVersion;
+	}
+
+	public void setCurrentVersion(String currentVersion) {
+		this.currentVersion = currentVersion;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	@PrePersist //Antes de la creación
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate //Ante de la actualización
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
+	
+	
+}
